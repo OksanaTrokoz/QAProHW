ea26d62dcc9982da5d1f472d63215396ca5079f0
Oksana Trokoz <oksana.trokoz.it@gmail.com>
2024-11-08 16:18:48 +0100

    HW 10.1

---
+++ test.js
@@ -0,0 +1,12 @@
+
+var arr = [
+    { userName: "Test", lastName: "Test", email: "test.test@gmail.com" },
+    { userName: "Dmitro", lastName: "Porohov", email: "dmitro.porohov@yahoo.com" },
+    { userName: "Andrii", lastName: "", email: "andrii@mail.ru" }
+];
+
+var trustedEmails = arr
+    .map(item => item.email) 
+    .filter(email => /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail\.com|yahoo\.com)$/.test(email)); 
+
+console.log(trustedEmails);
